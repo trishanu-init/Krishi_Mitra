5acc4339aa15f84c06d04f51a683ad73079c3d3a
trishanu-init <trishanu.nayak@outlook.com>
2024-09-01 20:39:48 +0530

    added frontend url in shop.js and seller.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://vendor-market-frontend.vercel.app/',],
+  origin: 'https://vendor-market-frontend.vercel.app/',
   credentials: true
 }));
 
