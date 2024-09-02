58f4e8779d5817a72a3cf645858d6d2d3a2adb24
trishanu-init <trishanu.nayak@outlook.com>
2024-09-02 12:24:47 +0530

    changed hero section

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -8,20 +8,18 @@ const Hero = () => {
       className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
       style={{
         backgroundImage:
-          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+          "url(https://i.ibb.co/jgjNbTq/Untitled-design.jpg)",
       }}
     >
       <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
         <h1
           className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
         >
-          Best Collection for <br /> home Decoration
+          Get Fresh, Local Produce <br /> Straight from Farmers
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
-          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
-          quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
-          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+        Empower Your Harvest: Sell Fresh, Buy Smart. <br /> Get the Best Tools and Fertilizers, Plus Plant Disease Detection.<br />
+        Revolutionize Your Farm with the Platform Built for Farmers
         </p>
         <Link to="/products" className="inline-block">
             <div className={`${styles.button} mt-5`}>
